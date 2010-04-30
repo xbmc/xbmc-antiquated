xbmc/xbmc-antiquated

commit 5b379d31066800b8c6c1b4254500668ca726576d

Ticket #9126: Force global destruction order + construction order to avoid crashes at exit. Thx CrystalP for initial patch
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29685 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/osx/WinSystemOSXGL.mm
@@ -25,9 +25,10 @@
 // and obj-c's typedef unsigned char BOOL
 #define BOOL XBMC_BOOL 
 #include "WinSystemOSXGL.h"
+#include "SystemGlobals.h"
 #undef BOOL
 
-CWinSystemOSXGL g_Windowing;
+CWinSystemOSXGL& g_Windowing = g_SystemGlobals.m_Windowing;
 
 CWinSystemOSXGL::CWinSystemOSXGL()
 {

guilib/GraphicContext.cpp
@@ -31,10 +31,11 @@
 #include "TextureManager.h"
 #include "MouseStat.h"
 #include "GUIWindowManager.h"
+#include "SystemGlobals.h"
 
 using namespace std;
 
-CGraphicContext g_graphicsContext;
+CGraphicContext& g_graphicsContext = g_SystemGlobals.m_graphicsContext;
 extern bool g_fullScreen;
 
 /* quick access to a skin setting, fine unless we starts clearing video settings */

guilib/GraphicContext.h
@@ -194,5 +194,5 @@ private:
  \ingroup graphics
  \brief
  */
-extern CGraphicContext g_graphicsContext;
+extern CGraphicContext& g_graphicsContext;
 #endif

xbmc/Application.cpp
@@ -24,6 +24,7 @@
 #endif
 #include "Application.h"
 #include "utils/Builtins.h"
+#include "SystemGlobals.h"
 #include "Splash.h"
 #include "KeyboardLayoutConfiguration.h"
 #include "LangInfo.h"
@@ -293,6 +294,8 @@ using namespace ANNOUNCEMENT;
 #define USE_RELEASE_LIBS
 
 #define MAX_FFWD_SPEED 5
+
+CApplication& g_application = g_SystemGlobals.m_application;
 
 //extern IDirectSoundRenderer* m_pAudioDecoder;
 CApplication::CApplication(void) : m_itemCurrentFile(new CFileItem), m_progressTrackingItem(new CFileItem)

xbmc/Application.h
@@ -371,4 +371,4 @@ protected:
 #endif
 };
 
-extern CApplication g_application;
+extern CApplication& g_application;

xbmc/MouseStat.cpp
@@ -24,8 +24,9 @@
 #include "WindowingFactory.h"
 #include "utils/log.h"
 #include "utils/TimeUtils.h"
+#include "SystemGlobals.h"
 
-CMouseStat g_Mouse;
+CMouseStat& g_Mouse = g_SystemGlobals.m_Mouse;
 
 CMouseStat::CButtonState::CButtonState()
 {

xbmc/MouseStat.h
@@ -165,7 +165,7 @@ private:
   int  bHold[5];
 };
 
-extern CMouseStat g_Mouse;
+extern CMouseStat& g_Mouse;
 
 #endif
 

xbmc/SystemGlobals.h
@@ -1,0 +1,56 @@
+#pragma once
+
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "WindowingFactory.h"
+#include "cores/VideoRenderers/RenderManager.h"
+#include "GraphicContext.h"
+#include "MouseStat.h"
+#include "Application.h"
+
+struct CSystemGlobals
+{
+  // Classes that must be initialized/destructed in a specific order because of dependencies.
+
+#if defined(_WIN32) && defined(HAS_GL)
+  CWinSystemWin32GL  m_Windowing;
+#endif
+
+#if defined(_WIN32) && defined(HAS_DX)
+  CWinSystemWin32DX  m_Windowing;
+#endif
+
+#if defined(__APPLE__)
+  CWinSystemOSXGL    m_Windowing;
+#endif
+
+#if defined(HAS_GLX)
+  CWinSystemX11GL    m_Windowing;
+#endif
+
+  CXBMCRenderManager m_renderManager;
+  CGraphicContext    m_graphicsContext;
+  CMouseStat         m_Mouse;
+  CApplication       m_application;
+};
+
+extern CSystemGlobals g_SystemGlobals;

commit 26ce4392379971749910e25cab8ffc3b0e5df6f5

fixed broken OSX compile caused by r29685
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29693 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

guilib/GUIWindow.cpp
@@ -721,6 +721,11 @@ bool CGUIWindow::ControlGroupHasFocus(int groupID, int controlID)
   return false;
 }
 
+bool CGUIWindow::HasID(int id)
+{
+  return (id >= m_controlID && id < m_controlID + m_idRange);
+}
+
 void CGUIWindow::SaveControlStates()
 {
   ResetControlStates();

guilib/GUIWindow.h
@@ -114,7 +114,7 @@ public:
   virtual bool OnMessage(CGUIMessage& message);
 
   bool ControlGroupHasFocus(int groupID, int controlID);
-  virtual bool HasID(int id) { return (id >= m_controlID && id < m_controlID + m_idRange); };
+  virtual bool HasID(int id);
   void SetIDRange(int range) { m_idRange = range; };
   int GetIDRange() const { return m_idRange; };
   int GetPreviousWindow() { return m_previousWindow; };

xbmc/osx/WinSystemOSXGL.mm
@@ -23,8 +23,10 @@
 
 //hack around problem with xbmc's typedef int BOOL
 // and obj-c's typedef unsigned char BOOL
-#define BOOL XBMC_BOOL 
+#define BOOL XBMC_BOOL
+#include "../guilib/Texture.h"
 #include "WinSystemOSXGL.h"
+#include "RenderSystemGL.h"
 #include "SystemGlobals.h"
 #undef BOOL
 