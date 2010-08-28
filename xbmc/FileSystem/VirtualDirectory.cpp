xbmc/xbmc-antiquated

commit 5eaa99a69b35117c895df39b5b3eb71eac0600fe

replaced detectmediatype by media manager and removed a conditional. OSX/Linux please test but this should work for you too.
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@21766 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/FileSystem/VirtualDirectory.cpp
@@ -236,18 +236,13 @@ void CVirtualDirectory::GetSources(VECSOURCES &shares) const
     CMediaSource& share = shares[i];
     if (share.m_iDriveType == CMediaSource::SOURCE_TYPE_DVD)
     {
-#ifdef _WIN32PC
-      if(CWIN32Util::IsAudioCD(share.strPath))
+      if(g_mediaManager.IsAudio(share.strPath))
       {
         share.strStatus = "Audio-CD";
         share.strPath = "cdda://local/";
       }
       else
-        share.strStatus = CWIN32Util::GetDiskLabel(share.strPath);
-#else
-      share.strStatus = MEDIA_DETECT::CDetectDVDMedia::GetDVDLabel();
-      share.strPath = MEDIA_DETECT::CDetectDVDMedia::GetDVDPath();
-#endif
+        share.strStatus = g_mediaManager.GetDiskLabel(share.strPath);
     }
   }
 }

commit c7e5e4e41cd7bdf316105ded5e5b1f7a0690dbca

fixed: using unmounted iso9660 discs on linux, broken in r21766
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33265 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/FileSystem/VirtualDirectory.cpp
@@ -227,7 +227,17 @@ void CVirtualDirectory::GetSources(VECSOURCES &shares) const
         share.strPath = "cdda://local/";
       }
       else
+      {
         share.strStatus = g_mediaManager.GetDiskLabel(share.strPath);
+        if (!share.strPath.length()) // unmounted CD
+        {
+          if (g_mediaManager.GetDiscPath() == "iso9660://")
+            share.strPath = "iso9660://";
+          else
+            // share is unmounted and not iso9660, discard it
+            shares.erase(shares.begin() + i--);
+        }
+      }
     }
   }
 #endif

xbmc/MediaManager.cpp
@@ -430,6 +430,16 @@ CStdString CMediaManager::GetDiskLabel(const CStdString& devicePath)
   return MEDIA_DETECT::CDetectDVDMedia::GetDVDLabel();
 #endif
 }
+
+CStdString CMediaManager::GetDiscPath()
+{
+#ifdef _WIN32
+  return "";
+#else
+  // iso9660://, cdda://local/ or D:\ depending on disc type
+  return MEDIA_DETECT::CDetectDVDMedia::GetDVDPath();
+#endif
+}
 #endif
 
 void CMediaManager::SetHasOpticalDrive(bool bstatus)

xbmc/MediaManager.h
@@ -72,6 +72,7 @@ public:
   bool RemoveCdInfo(const CStdString& devicePath="");
 #endif
   CStdString GetDiskLabel(const CStdString& devicePath="");
+  CStdString GetDiscPath();
   void SetHasOpticalDrive(bool bstatus);
 
   bool Eject(CStdString mountpath);